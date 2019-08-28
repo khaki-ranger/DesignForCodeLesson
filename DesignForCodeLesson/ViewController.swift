9d3166713d70d831d6c82bc40bf385c1d66d8cc0
Terashima <yamanogomora@gmail.com>
2019-08-28 19:00:04 +0900

    Edit: デバイスの向きが変更した時などに対応するための記述方法

--- DesignForCodeLesson/ViewController.swift
+++ DesignForCodeLesson/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var label: UILabel!
+    var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // ラベルを生成する
+        label = UILabel()
+        // ラベルのテキストを指定する
+        label.text = "Hello!"
+        // ラベルのテキストを中央寄せにする
+        label.textAlignment = NSTextAlignment.center
+        // ラベルのテキストカラーを変える
+        label.textColor = UIColor.white
+        // ラベルの背景色を変える
+        label.backgroundColor = UIColor.red
+        // ラベルをViewControllerのviewの上に乗せる
+        view.addSubview(label)
+
+        // ボタンを生成
+        button = UIButton()
+        // ボタンの背景色を変える
+        button.backgroundColor = UIColor.black
+        // ボタンをViewControllerのviewの上に乗せる
+        view.addSubview(button)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // このメソッド内でsafeAreaInsetsを元にレイアウトを組む
+        // ラベルのレイアウトを決める
+        label.frame = CGRect(x: view.frame.origin.x + 50, y: 50, width: view.frame.width - 50 * 2, height: 100)
+        // ボタンのレイアウトを決める
+        button.frame = CGRect(x: label.frame.maxX + 10, y: label.frame.maxY + 30, width: 30, height: 100)
     }
 
     override func didReceiveMemoryWarning() {
